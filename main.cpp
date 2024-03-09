c1ae0bae706e3d68cd066dc0d151f012769c4b0f
angelourias <angelou02005@gmail.com>
2024-03-08 20:42:45 -0800

    hw

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	int n1, n2;
-	int N;
+int main() {
+    int n1, n2, N;
 
-	cin >> n1 >> n2;
-	cin >> N;
+    cin >> n1 >> n2;
 
-	/***************************************************
-	 * Code your program here
-	 **************************************************	*/
+    cin >> N;
+
+    cout << n1 << " " << n2 << " ";
+
+    for (int i = 2; i < N; ++i) {
+        int next = n1 + n2;
+        cout << next << " ";
+        n1 = n2;
+        n2 = next;
+    }
+
+    cout << endl;
+
+    return 0;
 }
